cbacf9126b3903cc319e56cedcacf3b4fa3e92b3
Gerard Colman <gerard.colman1@gmail.com>
2021-11-08 21:30:42 +0000

    added MAX SIZE macro

--- MLP.h
+++ MLP.h
@@ -2,19 +2,20 @@
     All array values are temporary until I figure that part out
     Might used linked list instead
 */
+#define MAX_SIZE 5000
 
 typedef struct MLP {
     int numberInputs;
     int numberHiddenUnits;
     int numberOutputs;
-    double weightLower[10][10];
-    double weightUpper[10][10];
-    double weightChangeLower[10][10];
-    double weightChangeUpper[10][10];
-    double activationsLower[10];
-    double activationsUpper[10];
-    double hiddenUnits[10];
-    double outputUnits[10];
+    double weightLower[MAX_SIZE][MAX_SIZE];
+    double weightUpper[MAX_SIZE][MAX_SIZE];
+    double weightChangeLower[MAX_SIZE][MAX_SIZE];
+    double weightChangeUpper[MAX_SIZE][MAX_SIZE];
+    double activationsLower[MAX_SIZE];
+    double activationsUpper[MAX_SIZE];
+    double hiddenUnits[MAX_SIZE];
+    double outputUnits[MAX_SIZE];
 } MultiLayedPerceptron;
 
 void MLP_setup(MultiLayedPerceptron *MLP);
