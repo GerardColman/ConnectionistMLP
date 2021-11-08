7a08c2e4eb4ea9c41cf7d1614eea671b8b500619
Gerard Colman <gerard.colman1@gmail.com>
2021-11-08 21:01:19 +0000

    initial commit

---
+++ MLP.h
@@ -0,0 +1,26 @@
+/*
+    All array values are temporary until I figure that part out
+    Might used linked list instead
+*/
+
+typedef struct MLP {
+    int numberInputs;
+    int numberHiddenUnits;
+    int numberOutputs;
+    double weightLower[10][10];
+    double weightUpper[10][10];
+    double weightChangeLower[10][10];
+    double weightChangeUpper[10][10];
+    double activationsLower[10];
+    double activationsUpper[10];
+    double hiddenUnits[10];
+    double outputUnits[10];
+} MultiLayedPerceptron;
+
+void MLP_randomise(MultiLayedPerceptron *MLP);
+
+void MLP_forward(double i, MultiLayedPerceptron *MLP);
+
+void MLP_double_backwards(double t, MultiLayedPerceptron *MLP);
+
+void MLP_updateWeights(double learningRate, MultiLayedPerceptron *MLP);
